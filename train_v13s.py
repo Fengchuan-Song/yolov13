40e60d2c4471afcde611aea3664a0f64cde7663a
Fengchuan-Song <1319825810@qq.com>
2026-05-17 16:52:14 +0800

    update

--- train_v13s.py
+++ train_v13s.py
@@ -22,7 +22,7 @@ def parse_args():
     parser.add_argument("--device", default="0", help="Training device, e.g. 0, 0,1, cpu")
     parser.add_argument("--workers", type=int, default=8, help="Number of dataloader workers")
     parser.add_argument("--project", default="runs/detect", help="Save results to project/name")
-    parser.add_argument("--name", default="yolov13", help="Experiment name")
+    parser.add_argument("--name", default="yolov13s", help="Experiment name")
     parser.add_argument("--no-wandb", action="store_true", help="Disable Weights & Biases logging")
     parser.add_argument("--wandb-project", default='Achelous++', help="Weights & Biases project name")
     parser.add_argument("--wandb-entity", default=None, help="Weights & Biases entity/team name")
